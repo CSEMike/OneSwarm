6f1989db83dceeff5b3a2e343f01b383d1fac546
Will Scott <willscott@gmail.com>
2011-12-27 15:30:32 -0800

    Correct syntax of a placeolder test for junit.

--- javatests/edu/washington/cs/oneswarm/test/ux/RemoteAccessTest.java
+++ javatests/edu/washington/cs/oneswarm/test/ux/RemoteAccessTest.java
@@ -1,10 +1,14 @@
 package edu.washington.cs.oneswarm.test.ux;
 
-public class RemoteAccessTest {
+import org.junit.Test;
 
-    // TODO(piatek): Enable remote access. Verify SSL page loads. Verify
-    // SSL-only
-    // interface elements.
+public class RemoteAccessTest {
+    @Test
+    public void testRemoteAccess() throws Exception {
+        // TODO(piatek): Enable remote access. Verify SSL page loads. Verify
+        // SSL-only
+        // interface elements.
+    }
 
     public static final void main(String[] args) throws Exception {
         // placeholder
