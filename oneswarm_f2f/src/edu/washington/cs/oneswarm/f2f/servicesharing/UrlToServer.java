628262ef4b510e48eb87eaa6a0f6d88aea06538d
DaemonF <nksemail@gmail.com>
2012-05-21 15:26:42 -0700

    Added Classes to represent a server's publicly avaliable details (eg. exit policy), its privately tracked performance stats and to decide which server to use to connect to a given host and port.

---
+++ oneswarm_f2f/src/edu/washington/cs/oneswarm/f2f/servicesharing/UrlToServer.java
@@ -0,0 +1,25 @@
+package edu.washington.cs.oneswarm.f2f.servicesharing;
+
+import java.util.Collections;
+import java.util.List;
+
+public class UrlToServer {
+    List<ServerPublicInfo> servers;
+
+    public UrlToServer(ServerPublicInfo[] servers) {
+        for (ServerPublicInfo server : servers)
+            addServer(server);
+    }
+
+    public void addServer(ServerPublicInfo server) {
+        servers.add(server);
+        Collections.sort(servers);
+    }
+
+    public ServerPublicInfo pickServer(String url, int port) {
+        for (ServerPublicInfo server : servers)
+            if (server.allowsConnectionTo(url, port))
+                return server;
+        return null;
+    }
+}
